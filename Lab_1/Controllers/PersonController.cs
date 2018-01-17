63b1ae25d8ed9209a0d6c4be7887023a1889cd52
eallgood <32801558+eallgood@users.noreply.github.com>
2018-01-17 10:56:23 -0800

    First commit

---
+++ Lab_1/Controllers/PersonController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Lab_1.Models;
+
+namespace Lab_1.Controllers
+{
+    public class PersonController : Controller
+    {
+        // GET: Person
+        public ActionResult Index()
+        {
+            Person person = new Person();
+            ViewBag.Title = "Person";
+            ViewBag.FirstName = person.FirstName = "Ethan";
+            ViewBag.LastName = person.LastName = "Allgood";
+            ViewBag.Age = person.Age = 26;
+
+            return View();
+        }
+    }
+}
